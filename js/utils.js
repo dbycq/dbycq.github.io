799170a70ae54dee8e64c572d6b9fe1952ee73b6
dbycq <florr_dbycq@163.com>
2025-10-26 19:22:34 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@ function getClient(){
     if (!supabaseClient) {
         const supabaseUrl = 'https://gidktxcpudzdaqwtkeqq.supabase.co';
         const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZGt0eGNwdWR6ZGFxd3RrZXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MzA5MTMsImV4cCI6MjA3NzAwNjkxM30.gvlcZgz7N0nM4-TzW1s58AxBoXXJeL15x7UIugGgjr8';
-        supabaseCilent = supabase.createClient(supabaseUrl, supabaseKey);
+        supabaseCilent = Supabase.createClient(supabaseUrl, supabaseKey);
     }
     return supabaseClient;
 }
