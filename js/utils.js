dd11b91b46d8ad09b0dbb6f3efb0fca24be86099
dbycq <florr_dbycq@163.com>
2025-10-26 19:41:01 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,16 @@ function getClient(){
     if (!supabaseClient) {
         const supabaseUrl = 'https://gidktxcpudzdaqwtkeqq.supabase.co';
         const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZGt0eGNwdWR6ZGFxd3RrZXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MzA5MTMsImV4cCI6MjA3NzAwNjkxM30.gvlcZgz7N0nM4-TzW1s58AxBoXXJeL15x7UIugGgjr8';
-        supabaseCilent = window.Supabase.createClient(supabaseUrl, supabaseKey);
+        if (window.Supabase) {
+      // 若控制台显示 window.Supabase 存在（大写 S）
+      supabaseClient = window.Supabase.createClient(supabaseUrl, supabaseKey);
+    } else if (window.supabase) {
+      // 若控制台显示 window.supabase 存在（小写 s）
+      supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+    } else {
+      // 库未加载成功，抛出明确错误
+      throw new Error("Supabase 库未加载，无法创建客户端");
+    }
     }
     return supabaseClient;
 }
